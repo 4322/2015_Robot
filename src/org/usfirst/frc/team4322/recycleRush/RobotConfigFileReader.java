61dbc3b76911a6fcecc01340d9c1df0b4578d772
Nico1999 <nxmq08@gmail.com>
2015-04-07 12:01:00 -0700

    Added Comment parsing to RobotConfigFileReader

--- src/org/usfirst/frc/team4322/recycleRush/RobotConfigFileReader.java
+++ src/org/usfirst/frc/team4322/recycleRush/RobotConfigFileReader.java
@@ -12,6 +12,7 @@ public class RobotConfigFileReader
 {
 	private static RobotConfigFileReader _instance = null;
 	private static Pattern arrayFinder = Pattern.compile("\\{\\s*([^}]+)\\s*\\}");
+	private static Pattern commentFinder = Pattern.compile(";.*");
     public final String CONFIG_FILE = "/home/lvuser/robotConfig.ini";
     private static Map<Class<?>,Method> primitiveMap = new HashMap<Class<?>,Method>();
     static {
@@ -49,7 +50,6 @@ public class RobotConfigFileReader
     public void runRobotFileReader()
     {
     	RobotLogger.getInstance().sendToConsole("Started Config Update.");
-    	// Maybe only read the files necessary for the competition mode..
         Properties p = new Properties();
         try
         {
@@ -62,97 +62,10 @@ public class RobotConfigFileReader
         }
     	try
     	{
-//    		RobotMap.LAST_BUILD_TIME = p.getProperty("LAST_BUILD_TIME");
-//    		/**
-//    		 * AUTO MODE
-//    		 */
-//    		// Autonomous Driving Limits
-//    		RobotMap.AUTONOMOUS_DRIVE_SPEED = Double.parseDouble(p.getProperty("AUTONOMOUS_DRIVE_SPEED"));
-//	    	RobotMap.AUTONOMOUS_REVERSE_SPEED = Double.parseDouble(p.getProperty("AUTONOMOUS_REVERSE_SPEED"));
-//	    	RobotMap.BACK_AWAY_FROM_TOTE_SPEED = Double.parseDouble(p.getProperty("BACK_AWAY_FROM_TOTE_SPEED"));
-//	    	// P constant for autonomous driving
-//	    	RobotMap.AUTONOMOUS_P_CONTROL_VALUE_GYRO = Double.parseDouble(p.getProperty("AUTONOMOUS_P_CONTROL_VALUE_GYRO"));
-//	    	// Autonomous Drive Encoder Values
-//	    	RobotMap.BACK_AWAY_FROM_TOTE_DISTANCE = Integer.parseInt(p.getProperty("BACK_AWAY_FROM_TOTE_DISTANCE"));
-//	    	RobotMap.AUTO_DRIVE_FORWARD_DISTANCE = Integer.parseInt(p.getProperty("AUTO_DRIVE_FORWARD_DISTANCE"));
-//	    	RobotMap.AUTO_DRIVE_STRAFE_DISTANCE = Integer.parseInt(p.getProperty("AUTO_DRIVE_STRAFE_DISTANCE"));
-//	    	RobotMap.AUTO_DRIVE_BACKWARD_WITH_TOTE_CONTAINER = Integer.parseInt(p.getProperty("AUTO_DRIVE_BACKWARD_WITH_TOTE_CONTAINER"));
-//	    	RobotMap.AUTO_DRIVE_BACKWARD_WITH_TOTE_CONTAINER_OVER_PLATFORM = Integer.parseInt(p.getProperty("AUTO_DRIVE_BACKWARD_WITH_TOTE_CONTAINER_OVER_PLATFORM"));
-//
-//	    	/**
-//	    	 * TELEOP DRIVE MODE
-//	    	 */
-//    		// Acceleration Ramps
-//	    	RobotMap.THROTTLE_RAMP = Double.parseDouble(p.getProperty("THROTTLE_RAMP"));
-//	    	RobotMap.STEERING_RAMP = Double.parseDouble(p.getProperty("STEERING_RAMP"));
-//	    	RobotMap.STRAFE_RAMP = Double.parseDouble(p.getProperty("STRAFE_RAMP"));
-//	    	// Drive Power Limits
-//	    	RobotMap.THROTTLE_LIMIT = Double.parseDouble(p.getProperty("THROTTLE_LIMIT"));
-//	    	RobotMap.STEERING_LIMIT = Double.parseDouble(p.getProperty("STEERING_LIMIT"));
-//	    	RobotMap.STRAFE_LIMIT = Double.parseDouble(p.getProperty("STRAFE_LIMIT"));
-//	    	// Controller deadband
-//	    	RobotMap.STEERING_DEADBAND = Double.parseDouble(p.getProperty("STEERING_DEADBAND"));
-//	    	RobotMap.THROTTLE_DEADBAND = Double.parseDouble(p.getProperty("THROTTLE_DEADBAND"));
-//	    	// P constants for driving
-//	    	RobotMap.TELEOP_P_CONTROL_VALUE_GYRO = Double.parseDouble(p.getProperty("TELEOP_P_CONTROL_VALUE_GYRO"));
-//	    	RobotMap.TELEOP_STRAFE_P_CONTROL_VALUE_GYRO = Double.parseDouble(p.getProperty("TELEOP_STRAFE_P_CONTROL_VALUE_GYRO"));
-//	    	// Accelerometer Deadband values per axis
-//	    	RobotMap.ACCELEROMETER_DEADBAND_X = Double.parseDouble(p.getProperty("ACCELEROMETER_DEADBAND_X"));
-//	    	RobotMap.ACCELEROMETER_DEADBAND_Y = Double.parseDouble(p.getProperty("ACCELEROMETER_DEADBAND_Y"));
-//	    	RobotMap.ACCELEROMETER_DEADBAND_Z = Double.parseDouble(p.getProperty("ACCELEROMETER_DEADBAND_Z"));
-//	    	RobotMap.ACCELEROMETER_DEADBAND_COUNTDOWN = Integer.parseInt(p.getProperty("ACCELEROMETER_DEADBAND_COUNTDOWN"));
-//	    	// dual rates (crawl mode) //motor power is divided by this value
-//	    	RobotMap.CREEP_DRIVE_SPEED = Double.parseDouble(p.getProperty("CREEP_DRIVE_SPEED"));
-//	    	RobotMap.THROTTLE_DUAL_RATE = Double.parseDouble(p.getProperty("THROTTLE_DUAL_RATE"));
-//	    	RobotMap.STEERING_DUAL_RATE = Double.parseDouble(p.getProperty("STEERING_DUAL_RATE"));
-//	    	RobotMap.STRAFE_DUAL_RATE = Double.parseDouble(p.getProperty("STRAFE_DUAL_RATE"));
-//	    	
-//	    	/**
-//	    	 * TELEOP AUTO ALIGNMENT
-//	    	 */
-//	    	// Auto Alignment Speeds
-//	    	RobotMap.AUTO_ALIGN_STRAFE_SPEED = Double.parseDouble(p.getProperty("AUTO_ALIGN_STRAFE_SPEED"));
-//	    	RobotMap.AUTO_ALIGN_DRIVE_FORWARD_SPEED = Double.parseDouble(p.getProperty("AUTO_ALIGN_DRIVE_FORWARD_SPEED"));
-//	    	RobotMap.AUTO_ALIGN_STRAFE_RAMP = Double.parseDouble(p.getProperty("AUTO_ALIGN_STRAFE_RAMP"));
-//	    	// Auto Alignment Distances
-//	    	RobotMap.STRAFE_ENCODER_TOTE_ALIGNMENT_DRIVE_DISTANCE = Double.parseDouble(p.getProperty("STRAFE_ENCODER_TOTE_ALIGNMENT_DRIVE_DISTANCE"));
-//	    	
-//	    	// Infrared Sensor Error
-//	    	RobotMap.PROXIMITY_SENSOR_ERROR_VALUE = Double.parseDouble(p.getProperty("PROXIMITY_SENSOR_ERROR_VALUE"));
-//	    	RobotMap.EXPECTED_TOTE_DISTANCE = Double.parseDouble(p.getProperty("EXPECTED_TOTE_DISTANCE"));
-//	    	RobotMap.MAX_EXPECTED_TOTE_DISTANCE = Integer.parseInt(p.getProperty("MAX_EXPECTED_TOTE_DISTANCE"));
-//	    	
-//	    	/**
-//	    	 * TELEOP PID Control
-//	    	 */
-//	    	// Tote Elevator PID Constants
-//	    	RobotMap.ELEVATOR_P_VALUE = Double.parseDouble(p.getProperty("ELEVATOR_P_VALUE"));
-//	    	RobotMap.ELEVATOR_I_VALUE = Double.parseDouble(p.getProperty("ELEVATOR_I_VALUE"));
-//	    	RobotMap.ELEVATOR_D_VALUE = Double.parseDouble(p.getProperty("ELEVATOR_D_VALUE"));
-//	    	RobotMap.ELEVATOR_F_VALUE = Integer.parseInt(p.getProperty("ELEVATOR_F_VALUE"));
-//	    	RobotMap.ELEVATOR_IZONE_VALUE = Integer.parseInt(p.getProperty("ELEVATOR_IZONE_VALUE"));
-//	    	RobotMap.ELEVATOR_RAMPRATE_VALUE = Integer.parseInt(p.getProperty("ELEVATOR_RAMPRATE_VALUE"));
-//	    	RobotMap.ELEVATOR_ANALOG_STICK_DEADBAND = Double.parseDouble(p.getProperty("ELEVATOR_ANALOG_STICK_DEADBAND"));
-//	    	// Tote Elevator positions
-//	    	RobotMap.ELEVATOR_JOYSTICK_DEADBAND = Double.parseDouble(p.getProperty("ELEVATOR_JOYSTICK_DEADBAND"));
-//	    	RobotMap.ELEVATOR_POSITIONS[0] = Integer.parseInt(p.getProperty("ELEVATOR_POSITION_0"));
-//	    	RobotMap.ELEVATOR_POSITIONS[1] = Integer.parseInt(p.getProperty("ELEVATOR_POSITION_1"));
-//	    	RobotMap.ELEVATOR_POSITIONS[2] = Integer.parseInt(p.getProperty("ELEVATOR_POSITION_2"));
-//	    	RobotMap.ELEVATOR_POSITIONS[3] = Integer.parseInt(p.getProperty("ELEVATOR_POSITION_3"));
-//	    	RobotMap.ELEVATOR_POSITIONS[4] = Integer.parseInt(p.getProperty("ELEVATOR_POSITION_4"));
-//	    	// Tote Stack positions
-//	    	RobotMap.ELEVATOR_STACK_POSITIONS[0] = Integer.parseInt(p.getProperty("ELEVATOR_STACK_POSITION_0"));
-//	    	RobotMap.ELEVATOR_STACK_POSITIONS[1] = Integer.parseInt(p.getProperty("ELEVATOR_STACK_POSITION_1"));
-//	    	RobotMap.ELEVATOR_STACK_POSITIONS[2] = Integer.parseInt(p.getProperty("ELEVATOR_STACK_POSITION_2"));
-//	    	RobotMap.ELEVATOR_STACK_POSITIONS[3] = Integer.parseInt(p.getProperty("ELEVATOR_STACK_POSITION_3"));
-//	    	RobotMap.ELEVATOR_STACK_POSITIONS[4] = Integer.parseInt(p.getProperty("ELEVATOR_STACK_POSITION_4"));
-//	    	// Container pickup positions
-//	    	RobotMap.ELEVATOR_CONTAINER_POSITIONS[0] = Integer.parseInt(p.getProperty("ELEVATOR_CONTAINER_POSITION_1"));
-//	    	RobotMap.ELEVATOR_CONTAINER_POSITIONS[1] = Integer.parseInt(p.getProperty("ELEVATOR_CONTAINER_POSITION_2"));
     		Enumeration<?> enumeration = p.propertyNames();
     	    while (enumeration.hasMoreElements()) {
     	        String key = (String) enumeration.nextElement();
-    	        String value = p.getProperty(key);
+    	        String value = commentFinder.matcher(p.getProperty(key)).replaceAll(""); //Strips Comments, which start with semicolons
     	        Field current = null;
     	        try
     	        {
@@ -166,20 +79,27 @@ public class RobotConfigFileReader
     	        }
     	        if(current.getType().isArray())
     	        {
+    	        	//use regex to parse array
     	        	Matcher m = arrayFinder.matcher(value);
     	        	m.find();
+    	        	//split array into strings
     	        	String[] arrayValues = m.group().split("[\\s,]+");
+    	        	//strip brackets
     	        	arrayValues[0] = arrayValues[0].replace("{", "");
     	        	arrayValues[arrayValues.length-1] = arrayValues[arrayValues.length-1].replace("}", "");
+    	        	//instantiate array
     	        	Object elementArray = Array.newInstance(current.getType().getComponentType(), arrayValues.length);
     	        	try
     	        	{
+    	        		//fill array
     	        		for(int i = 0; i< arrayValues.length;i++)
     	        		{
+    	        			//if the array is made of strings, set the array value to the raw string
     	        			if(current.getType().getComponentType() == String.class)
     	        			{
     	        				Array.set(elementArray, i,arrayValues[i]);
     	        			}
+    	        			//if not, cast it and then set
     	        			else
     	        			{
     	        				Array.set(elementArray, i, primitiveMap.get(current.getType().getComponentType()).invoke(null, arrayValues[i]));
